68ce4655b7e89de76e9d9c6940e68bae2a561ad4
mayankbisht-tech <mayankbisht1107@gmail.com>
2024-11-12 21:47:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function showSection(sectionId) {
+    document.querySelectorAll('.section').forEach(section => {
+        section.style.display = 'none';
+    });
+    document.getElementById(sectionId).style.display = 'block';
+}
+
+window.onload = () => {
+    showSection('home');
+};
